5e0488f46ec6151502cc7390728c79a856bb3390
Dmitry Taskaev <89737095+DmiTsa@users.noreply.github.com>
2021-08-31 16:33:35 +0300

    temp
    temp

--- src/Basic1/Main.java
+++ src/Basic1/Main.java
@@ -1,8 +1,9 @@
 package Basic1;
+//найдите значение функции z=((a-3)*b/2)+c
 
 public class Main {
     public static void main(String[] args) {
-        float z;
+        int z;
         int a=5,b=4,c=3;
         z= (a-3)*b/2 + c;
         System.out.print(z);
