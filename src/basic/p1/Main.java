18cb71082a282f732aed4ca7b3ebae75eaf2e972
Dmitry Taskaev <89737095+DmiTsa@users.noreply.github.com>
2021-09-01 14:49:20 +0300

    01-09-21
    add basic. package (move in)

---
+++ src/basic/p1/Main.java
@@ -0,0 +1,10 @@
+package basic.p1;
+//найдите значение функции z=((a-3)*b/2)+c
+public class Main {
+    public static void main(String[] args) {
+        int z;
+        int a=5,b=4,c=3;
+        z= (a-3)*b/2 + c;
+        System.out.print(z);
+    }
+}
---
+++ src/basic/p8/Main.java
@@ -0,0 +1,26 @@
+package basic.p8;
+/*
+Найти max{min(a, b), min(c, d)}
+ */
+public class Main {
+    public static void main(String[] args) {
+    int min1, min2, max;
+    int a=2, b=4, c=-2, d=14;
+    if (a<b) {
+        min1 = a;
+    } else {
+        min1 = b;
+    }
+    if (c<d) {
+        min2 = c;
+    } else {
+        min2 = d;
+    }
+    if (min1 > min2) {
+        max = min1;
+    } else {
+        max = min2;
+    }
+        System.out.println(max);
+    }
+}
