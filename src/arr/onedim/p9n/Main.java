4aab1e916b08b0f63938038b4dcb558116ad6f7f
Dmitry Taskaev <89737095+DmiTsa@users.noreply.github.com>
2021-09-07 08:47:24 +0300

    07-09-21

--- src/arr/onedim/p9n/Main.java
+++ src/arr/onedim/p9n/Main.java
@@ -1,8 +1,30 @@
 package arr.onedim.p9n;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
 В массиве целых чисел с количеством элементов n найти наиболее часто встречающееся число. Если таких
 чисел несколько, то определить наименьшее из них.
  */
 public class Main {
+    public static int[] getArrayInt(char print) {
+        System.out.println("Введите размерность массива");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Введите элементы массива");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        if (print == 'y') System.out.println(Arrays.toString(arr));
+        return arr;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = getArrayInt('y');
+
+    }
 }
+
