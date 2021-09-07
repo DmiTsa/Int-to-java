f433f7960b6f26a7bf2fa0cdd80b8b36474b399c
Dmitry Taskaev <89737095+DmiTsa@users.noreply.github.com>
2021-09-07 15:16:49 +0300

    07-09-21n

--- src/arr/onedim/p10/Main.java
+++ src/arr/onedim/p10/Main.java
@@ -1,10 +1,17 @@
-package arr.onedim.p10n;
+package arr.onedim.p10;
+
+import java.util.Arrays;
+
 /*
 Дан целочисленный массив с количеством элементов п. Сжать массив, выбросив из него каждый второй
 элемент (освободившиеся элементы заполнить нулями). Примечание. Дополнительный массив не использовать.
  */
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr = new int[]{5, 6, 8, 7, 2, 14, 12, -5, 1, -2};
+        for (int i = 1; i < arr.length; i = i + 2) {
+            arr[i] = 0;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
--- src/arr/onedim/p9n/Main.java
+++ src/arr/onedim/p9n/Main.java
@@ -1,30 +1,49 @@
-package arr.onedim.p9n;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Scanner;
-
-/*
-В массиве целых чисел с количеством элементов n найти наиболее часто встречающееся число. Если таких
-чисел несколько, то определить наименьшее из них.
- */
-public class Main {
-    public static int[] getArrayInt(char print) {
-        System.out.println("Введите размерность массива");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Введите элементы массива");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        if (print == 'y') System.out.println(Arrays.toString(arr));
-        return arr;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = getArrayInt('y');
-
-    }
-}
-
+///*
+//package arr.onedim.p9n;
+//
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//*/
+///*
+//В массиве целых чисел с количеством элементов n найти наиболее часто встречающееся число. Если таких
+//чисел несколько, то определить наименьшее из них.
+// *//*
+//
+//public class Main {
+//    public static int[] getArrayInt() {
+//        System.out.println("Введите размерность массива");
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        System.out.println("Введите элементы массива");
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = sc.nextInt();
+//        }
+//        return arr;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = getArrayInt();
+//        int[] countElement = new int[arr.length];
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = i; j < arr.length; j++) {
+//                if (arr[i] == arr[j]) countElement[i]++;
+//            }
+//        }
+//       // System.out.println(Arrays.toString(countElement));//////
+//
+//        int maxCount = 0;
+//        int max=0;
+//        for (int i = 1; i < countElement.length; i++)
+//            if (countElement[i] >= maxCount) && (maxCount = countElement[i];
+//
+//        for (int i = 0; i < arr.length; i++)
+//     if ((countElement[i] == maxCount ) && (arr[i]>max)){
+//         arr[i];
+//     }
+//
+//
+//            //System.out.println("макс приоритет = " + maxCount);
+//    }
+//}*/
