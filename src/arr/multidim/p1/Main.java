542741e9e0c410110540dd2e694cf3f17e0b5789
Dmitry Taskaev <89737095+DmiTsa@users.noreply.github.com>
2021-09-08 14:49:05 +0300

    08-09-21w

--- src/arr/multidim/p1/Main.java
+++ src/arr/multidim/p1/Main.java
@@ -1,4 +1,11 @@
 package arr.multidim.p1;
 
+/*
+ Дана матрица. Вывести на экран все нечетные столбцы, у которых первый элемент больше последнего
+ */
 public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {{4, 6, 8, 0, 8}, {4, 6, 5, 14, 1}};
+
+    }
 }
