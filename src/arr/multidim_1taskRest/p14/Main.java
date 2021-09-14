d8621dec58ec4c5fcb0d6146df11d0ff30e4ca69
Dmitry Taskaev <89737095+DmiTsa@users.noreply.github.com>
2021-09-14 21:57:02 +0300

    14-09-21h

---
+++ src/arr/multidim_1taskRest/p14/Main.java
@@ -0,0 +1,30 @@
+package arr.multidim_1taskRest.p14;
+
+/*
+Сформировать случайную матрицу m x n, состоящую из нулей и единиц, причем в каждом столбце число
+единиц равно номеру столбца.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int m = 14, n = 6;
+        int[][] matrix = new int[m][n];
+        int mx;
+        for (int j = 1; j < n; j++) {
+            int count = 0;
+            do {
+                do {
+                    mx = ((int) (Math.random() * m));
+                } while (matrix[mx][j] != 0);
+
+                matrix[mx][j] = 1;
+                count++;
+            } while (count != j);
+        }
+        for (int[] arr : matrix) {
+            for (int elem : arr) {
+                System.out.print(elem + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/arr/multidim_1taskRest/p9/Main.java
@@ -0,0 +1,27 @@
+package arr.multidim_1taskRest.p9;
+
+/*
+Задана матрица неотрицательных чисел. Посчитать сумму элементов в каждом столбце. Определить, какой
+столбец содержит максимальную сумму.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {{2, 7, 6,9},
+                {30, 12, 1,7},
+                {12, 4, 33,100}};
+        int sum = 0;
+        int tmp,column=0;
+        for (int j=0; j < 4; j++) {
+            tmp = 0;
+            for (int i = 0; i < 3; i++) {
+                tmp += matrix[i][j];
+            }
+            System.out.println("Сумма столбца равна " + tmp);
+            if (tmp > sum) {
+                sum = tmp;
+                column = j;
+            }
+        }
+        System.out.println("Сумма = " + sum +" "+ "столбец " + (column+1));
+    }
+}
